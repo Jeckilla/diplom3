e012486297cc02241b72dd50a67f2c32b2336563
jeckilla <jeckilla@gmail.com>
2024-04-17 16:19:01 +0300

    Created models and admin panel

---
+++ orders/backend/admin.py
@@ -0,0 +1,82 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import (User, Shop, Category, Product, ProductInfo, Parameter, ProductParameter, Order, OrderItem, \
+                 ConfirmEmailToken, Contact)
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    """
+    Панель управления пользователями
+    """
+    model = User
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'type')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'company', 'position')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    list_display = ['email', 'first_name', 'last_name', 'is_staff']
+
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'url']
+    list_filter = ['name',]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_filter = ['name',]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category']
+    list_filter = ['category',]
+
+
+@admin.register(ProductInfo)
+class ProductInfoAdmin(admin.ModelAdmin):
+    list_display = ['product', 'shop', 'quantity', 'price', 'price_rrc']
+    list_filter = ['shop', 'quantity', 'price']
+
+
+@admin.register(Parameter)
+class ParameterAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(ProductParameter)
+class ProductParameterAdmin(admin.ModelAdmin):
+    list_display = ['product_info', 'parameter', 'value']
+    list_filter = ['parameter',]
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'state', 'contact']
+    list_filter = ['user', 'state']
+
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['order', 'product', 'shop', 'quantity']
+    list_filter = ['order', 'shop', 'quantity']
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['user', 'city', 'street', 'phone']
+    list_filter = ['user', 'city', 'street']
+
+
+@admin.register(ConfirmEmailToken)
+class ConfirmEmailTokenAdmin(admin.ModelAdmin):
+    list_display = ['user', 'key', 'created_at',]
---
+++ orders/backend/models.py
@@ -0,0 +1,278 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.db import models
+from django_rest_passwordreset.tokens import get_token_generator
+
+STATE_CHOICES = (
+    ('basket', 'Статус корзины'),
+    ('new', 'Новый'),
+    ('confirmed', 'Подтвержден'),
+    ('assembled', 'Собран'),
+    ('sent', 'Отправлен'),
+    ('delivered', 'Доставлен'),
+    ('canceled', 'Отменен'),
+)
+
+USER_TYPE_CHOICES = (
+    ('shop', 'Магазин'),
+    ('buyer', 'Покупатель'),
+)
+
+
+class UserManager(BaseUserManager):
+    """
+    Миксин для управления пользователями
+    """
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    """
+    Стандартная модель пользователей
+    """
+    REQUIRED_FIELDS = []
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    email = models.EmailField(verbose_name='email', max_length=254, blank=False,
+                              unique=True, error_messages={'unique': 'Пользователь с таким email уже существует'})
+    company = models.CharField(verbose_name='Компания', max_length=40, blank=True)
+    position = models.CharField(verbose_name='Должность', max_length=40, blank=True)
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(
+        verbose_name='username',
+        max_length=150,
+        help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.',
+        validators=[username_validator],
+        error_messages={
+            'unique': "A user with that username already exists.",
+        },
+    )
+    is_active = models.BooleanField(
+        verbose_name='active',
+        default=False,
+        help_text=(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+    type = models.CharField(verbose_name='Тип пользователя', choices=USER_TYPE_CHOICES, max_length=5, default='buyer')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = "Список пользователей"
+        ordering = ('email',)
+
+
+class Shop(models.Model):
+    objects = models.manager.Manager()
+    name = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = 'Магазин'
+        verbose_name_plural = "Список магазинов"
+        ordering = ('-name',)
+
+
+class Category(models.Model):
+    objects = models.manager.Manager()
+    name = models.CharField(max_length=255)
+    shops = models.ForeignKey(Shop, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = "Список категорий"
+        ordering = ('-name',)
+
+
+class Product(models.Model):
+    objects = models.manager.Manager()
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = "Список продуктов"
+        ordering = ('-name',)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductInfo(models.Model):
+    objects = models.manager.Manager()
+    product = models.ForeignKey(Product, verbose_name='Продукт', related_name='product_info', on_delete=models.CASCADE)
+    shop = models.ForeignKey(Shop, verbose_name='Магазин', related_name='product_info', on_delete=models.CASCADE)
+    model = models.CharField(max_length=80, verbose_name='Модель', blank=True)
+    external_id = models.CharField(max_length=80, verbose_name='Внешний ИД')
+    quantity = models.PositiveIntegerField(verbose_name='Количество')
+    price = models.PositiveIntegerField(verbose_name='Количество')
+    price_rrc = models.PositiveIntegerField(verbose_name='Количество')
+
+    class Meta:
+        verbose_name = 'Информация о продукте'
+        verbose_name_plural = "Список информации о продуктах"
+        constraints = [
+            models.UniqueConstraint(fields=['product', 'shop', 'external_id'], name='unique_product_info'),
+        ]
+
+
+class Parameter(models.Model):
+    objects = models.manager.Manager()
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = 'Параметр'
+        verbose_name_plural = "Список параметров"
+        ordering = ('-name',)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductParameter(models.Model):
+    objects = models.manager.Manager()
+    product_info = models.ForeignKey(ProductInfo,
+                                     verbose_name='Информация о продукте',
+                                     related_name='product_parameters',
+                                     blank=True, on_delete=models.CASCADE)
+    parameter = models.ForeignKey(Parameter,
+                                  verbose_name='Параметр',
+                                  related_name='product_parameters',
+                                  blank=True, on_delete=models.CASCADE)
+    value = models.CharField(max_length=255, verbose_name='Значение')
+
+    class Meta:
+        verbose_name = 'Параметр'
+        verbose_name_plural = "Список параметров"
+        constraints = [
+            models.UniqueConstraint(fields=['product_info', 'parameter'], name='unique_product_parameter'),
+        ]
+
+
+class Contact(models.Model):
+    objects = models.manager.Manager()
+    user = models.ForeignKey(User, verbose_name="Пользователь", related_name="contacts",
+                             blank=True, on_delete=models.CASCADE)
+    city = models.CharField(max_length=50, verbose_name='Город')
+    street = models.CharField(max_length=100, verbose_name='Улица')
+    house = models.CharField(max_length=15, verbose_name='Дом', blank=True)
+    structure = models.CharField(max_length=15, verbose_name='Корпус', blank=True)
+    building = models.CharField(max_length=15, verbose_name='Строение', blank=True)
+    apartment = models.CharField(max_length=15, verbose_name='Квартира', blank=True)
+    phone = models.CharField(max_length=20, verbose_name='Телефон')
+
+    class Meta:
+        verbose_name = 'Контакты пользователя'
+        verbose_name_plural = "Список контактов пользователя"
+
+    def __str__(self):
+        return f'Заказчик: {self.user}, {self.city}, {self.street}, {self.house}, {self.apartment}.'
+
+
+class Order(models.Model):
+    objects = models.manager.Manager()
+    user = models.ForeignKey(User, verbose_name='Пользователь',
+                             related_name='orders', blank=True,
+                             on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создан')
+    state = models.CharField(verbose_name="Статус", choices=STATE_CHOICES, max_length=15)
+    contact = models.ForeignKey(Contact, verbose_name='Контакты', blank=True, null=True, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = "Список заказов"
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return f'Заказ {self.id} от {self.created_at} статус {self.state}'
+
+
+class OrderItem(models.Model):
+    objects = models.manager.Manager()
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+
+    class Meta:
+        verbose_name = 'Заказанный продукт'
+        verbose_name_plural = "Список заказанных продуктов"
+        constraints = [
+            models.UniqueConstraint(fields=['order', 'product'], name='unique_order_item'),
+        ]
+
+
+class ConfirmEmailToken(models.Model):
+    objects = models.manager.Manager()
+
+    class Meta:
+        verbose_name = 'Токен подтверждения Email'
+        verbose_name_plural = 'Токены подтверждения Email'
+
+    @staticmethod
+    def generate_key():
+        """ generates a pseudo random code using os.urandom and binascii.hexlify """
+        return get_token_generator().generate_token()
+
+    user = models.ForeignKey(
+        User,
+        related_name='confirm_email_tokens',
+        on_delete=models.CASCADE,
+        verbose_name="The User which is associated to this password reset token"
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="When was this token generated"
+    )
+
+    # Key field, though it is not the primary key of the model
+    key = models.CharField(
+        verbose_name="Key",
+        max_length=64,
+        db_index=True,
+        unique=True
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = self.generate_key()
+        return super(ConfirmEmailToken, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Password reset token for user {self.user}"
